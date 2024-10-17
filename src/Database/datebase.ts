f1dcc767ed7857d5c8a05e3b281375534eca8395
Víctor Tomas Molina Arias <89616038+tomasmolinaarias@users.noreply.github.com>
2024-10-16 23:06:25 -0300

    Update datebase.ts

--- src/Database/datebase.ts
+++ src/Database/datebase.ts
@@ -7,7 +7,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER || 'use',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'ermanake',
-  port: Number(process.env.DB_PORT) || 3307,
+  port: Number(process.env.DB_PORT) || 3306,
   logging: false,
   models: [RolesUsuarios],  // Agrega los modelos aquí
 });
