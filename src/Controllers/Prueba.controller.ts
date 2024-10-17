f497b4863cc5149ae20e10501c58e7f9e427f4a5
yeranaika <darca604@gmail.com>
2024-10-16 22:56:29 -0300

    prueba ediv

--- src/Controllers/Prueba.controller.ts
+++ src/Controllers/Prueba.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 const Prueba = {
   leer: async (req: Request, res: Response): Promise<Response | void> => {
     try {
-      console.log("Ruta de prueba ejecutada 🟢");
+      console.log("Ruta de prueba ejecutad XDDDD 🟢");
       return res.send("Ruta de prueba ejecutada correctamente 🟢");
     } catch (error) {
       console.error("Error ", error);
